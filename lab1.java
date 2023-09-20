87f0be4f17ddd40e5fe89b523f13c8fd0fc83773
StalinAndPutin <78565749+StalinAndPutin@users.noreply.github.com>
2023-09-20 14:52:21 +0300

    Update lab1.java

--- lab1.java
+++ lab1.java
@@ -14,12 +14,16 @@ public  class lab1{
         double[][] c1 = new double[13][14];
         for (int i = 0; i<13; ++i){
             for (int j = 0; j<14; ++j){
-                if (c[i] == 8){
-                    c1[i][j]= Math.pow(Math.asin(1.0/(Math.exp(Math.abs(x[j])))), (Math.cos(Math.atan((x[j]-4)/16.0)))-4)/Math.cos(Math.exp(x[j]));
-                } else if (c[i] == 6 || c[i] == 9 || c[i] == 10 || c[i] == 11 || c[i] == 13 || c[i] == 14){
-                    c1[i][j] = Math.sin(Math.pow(x[j]*(4+x[j]),3))*(((Math.log(Math.abs(x[j])) + 2)/Math.cos(x[j])) - 1.0/3);
-                } else {
-                    c1[i][j] = Math.pow(1.0/2*(Math.exp(Math.atan(Math.sin(x[j]))) + 1), Math.PI-Math.tan(Math.pow(x[j],x[j]*(x[j]-1))));
+                switch(c[i]){
+                    case 8:
+                        c1[i][j]= Math.pow(Math.asin(1.0/(Math.exp(Math.abs(x[j])))), (Math.cos(Math.atan((x[j]-4)/16.0)))-4)/Math.cos(Math.exp(x[j]));
+                        break; 
+                    case 6,9,10,11,13,14:
+                        c1[i][j] = Math.sin(Math.pow(x[j]*(4+x[j]),3))*(((Math.log(Math.abs(x[j])) + 2)/Math.cos(x[j])) - 1.0/3);
+                        break;
+                    default:
+                        c1[i][j] = Math.pow(1.0/2*(Math.exp(Math.atan(Math.sin(x[j]))) + 1), Math.PI-Math.tan(Math.pow(x[j],x[j]*(x[j]-1))));
+                        break;
                 }
             }
         }
@@ -28,7 +32,6 @@ public  class lab1{
                 System.out.printf("%.2f ", c1[i][j]);
             }
             System.out.println();
-	    System.out.println();
         }
     }
 }
